7fcef971fba85b7d9876b360a75147f032e989c1
onenewcode <lovestudy@qq.com>
2025-03-21 19:20:39 +0800

    添加cv案例

--- motor/main.py
+++ motor/main.py
@@ -2,7 +2,6 @@ from dora import Node
 from Motor import _motor
 import pyarrow as pa
 
-# 字符串进行传递移动状态和方向，这里使用 scalar 进行抓换和传递
 # Stop
 # Advance
 # Back
@@ -25,14 +24,21 @@ import pyarrow as pa
 # # 右旋转
 # Rotate_Right
 
-
+# 使用数值作为输入 dataw 为单纯的数值，具体参考 motor.py
 def main():
     node = Node()
+    # 初始化电机
+    Control_Motor = _motor(1500, 1500, 1500, 1500)
+    Control_Motor.Car_run = 0
+
     for event in node:
+        # 控制车的循环，也许需要加入睡眠机制防止短时间大量指令
         if event["type"] == "INPUT":
             event_id = event["id"]
             if event_id == "move":
                 data = event["value"]
+                # TODO: 确认是否成功，之后找到测试方法
+                Control_Motor.Car_run = data
 
 
 if __name__ == "__main__":
